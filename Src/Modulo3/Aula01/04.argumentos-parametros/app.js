5ef42afccff288036955208de52eced68e4cc43a
bruna simoes moita <bruna.simoesmoita@gmail.com>
2022-12-08 01:03:46 -0300

    funcoes e exercicios

--- Src/Modulo3/Aula01/04.argumentos-parametros/app.js
+++ Src/Modulo3/Aula01/04.argumentos-parametros/app.js
@@ -1,9 +1,7 @@
 // Argumentos, parâmetros e default parameters
-//parametros
 const myFunc = function (name = 'Estrôncio', lastName = 'Almeida') {
   console.log(`Oi, ${name} ${lastName}!`)
 }
 
 myFunc()
-//argumentos
 myFunc('Roger')
---
+++ Src/Modulo3/Aula01/05.return/app.js
@@ -0,0 +1,12 @@
+// Retornando valores
+const double = function (number) {
+  return number * 2
+}
+
+const result = double(3)
+
+const showResult = function (value) {
+  return `O resultado é ${value}`
+}
+
+console.log(showResult(result))
